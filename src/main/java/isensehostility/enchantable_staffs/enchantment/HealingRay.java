12f40f20ceae352228756d1e39fbb86bcf298349
ISenseHostility <yaboijarno@gmail.com>
2022-07-13 00:15:41 +0200

    Added Healing Ray Enchantment

---
+++ src/main/java/isensehostility/enchantable_staffs/enchantment/HealingRay.java
@@ -0,0 +1,105 @@
+package isensehostility.enchantable_staffs.enchantment;
+
+import isensehostility.enchantable_staffs.config.StaffConfig;
+import isensehostility.enchantable_staffs.enchantment.category.StaffCategory;
+import isensehostility.enchantable_staffs.enums.EElement;
+import isensehostility.enchantable_staffs.item.Staff;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+import static isensehostility.enchantable_staffs.StaffUtils.getEntitiesInBreathLine;
+import static isensehostility.enchantable_staffs.StaffUtils.invokeStaffCosts;
+
+public class HealingRay extends Enchantment implements IStaffEnchantment {
+    public HealingRay() {
+        super(Rarity.UNCOMMON, StaffCategory.getInstance(), new EquipmentSlot[]{EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND});
+    }
+
+    @Override
+    public EElement[] getElements() {
+        return new EElement[]{EElement.PURE};
+    }
+
+    @Override
+    public boolean doesExist() {
+        return StaffConfig.healingRayExists.get();
+    }
+
+    @Override
+    public int getChargeCost() {
+        return StaffConfig.healingRayChargeCost.get();
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> onUse(ItemStack stack, Level level, Player player) {
+        if (invokeStaffCosts(player, stack, getChargeCost(), level)) {
+            return new InteractionResultHolder<>(InteractionResult.PASS, stack);
+        }
+
+        List<LivingEntity> entities = getEntitiesInBreathLine(level, player, 12, ParticleTypes.HAPPY_VILLAGER);
+
+        for (LivingEntity entity : entities) {
+            if (entity != player) {
+                entity.heal(4);
+                entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION,200));
+            }
+        }
+
+        level.playSound(null, new BlockPos(player.getEyePosition()), SoundEvents.CAT_AMBIENT, SoundSource.PLAYERS, 100.0F, 1.0F);
+
+        return new InteractionResultHolder<>(InteractionResult.SUCCESS, stack);
+    }
+
+    @Override
+    public int getMinLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getMinCost(int level) {
+        return 10;
+    }
+
+    @Override
+    public int getMaxCost(int level) {
+        return 50;
+    }
+
+    @Override
+    protected boolean checkCompatibility(Enchantment enchantment) {
+        return super.checkCompatibility(enchantment)
+                && enchantment == Enchantments.MENDING
+                && enchantment == Enchantments.UNBREAKING;
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack stack) {
+        return stack.getItem() instanceof Staff && doesExist();
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack) {
+        return this.canEnchant(stack);
+    }
+}
